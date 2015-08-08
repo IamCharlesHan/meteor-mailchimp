3bae2f17e02527e99174c3489b415fa6dd59e70f
Charles Changmin Han <name.cmh@gmail.com>
2015-08-07 17:27:40 -0700

    Update version number.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name    : 'miro:mailchimp',
-	version : '1.1.0',
+	version : '1.1.1',
 	summary : 'A Meteor wrapper for MailChimp API',
 	homepage: 'http://mailchimp.meteor.com',
 	author  : 'Miroslav Hibler (http://miro.hibler.me)',
